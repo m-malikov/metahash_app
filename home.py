832cbe02dd02a55b187861588f906570ca829078
m-malikov <malikov.ma@phystech.edu>
2018-09-30 03:14:15 +0300

    home.py bugfixes

--- home.py
+++ home.py
@@ -7,28 +7,35 @@ import json
 import serial
 
 hub_address = mhutils.get_address("keys/hub.pub")
-home_address = mhutils.get_address("keys/homr.pub")
-usb_input = serial.Serial('ttyAMX0', 9600)
+home_address = mhutils.get_address("keys/temp.pub")
+usb_input = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
     # data = json.dumps(
     #    {"temp": "22.5", "humidity": "100", "soil": "50"})
     usb_input.flushInput()
     data = usb_input.readline().decode('utf-8')
-    pprint(json.loads(data))
-    transaction_result = subprocess.check_output([
-        "python3",
-        "metahash.py",
-        "send-tx",
-        "--net=test",
-        "--pubkey=keys/temp.pub",
-        "--privkey=keys/temp.priv",
-        "--value=1",
-        "--to={}".format(hub_address),
-        '--data=' + data
-    ]).decode("utf-8")
-    print(transaction_result)
+    try:
+        pprint(json.loads(data))
+        transaction_result = subprocess.check_output([
+            "python3",
+            "metahash.py",
+            "send-tx",
+            "--net=test",
+            "--pubkey=keys/temp.pub",
+            "--privkey=keys/temp.priv",
+            "--value=1",
+            "--to={}".format(hub_address),
+            '--data=' + data
+        ]).decode("utf-8")
+        print(transaction_result)
+    except:
+        print(data)
 
     history = mhutils.get_history(home_address)
     last_command = mhutils.get_last_data_from_address(history, hub_address)
-    usb_input.write(last_command + '\n')
+    print(last_command)
+    if last_command == 'LIGHT_ON':
+        usb_input.write(b'A')
+    if last_command == 'LIGHT_OFF':
+        usb_input.write(b'B')
